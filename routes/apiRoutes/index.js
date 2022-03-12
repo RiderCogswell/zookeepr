809ccc1f6b13fc05242832e0e27998e09f24ad93
Ridercogswell <ridercogswell@gmail.com>
2022-03-12 14:31:04 -0800

    add seperated routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,7 @@
+// apiRoutes/index.js is a central hub for all routing functions
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+
+router.use(animalRoutes);
+
+module.exports = router;
